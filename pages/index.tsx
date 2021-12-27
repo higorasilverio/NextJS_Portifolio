3475a6014552582b41dc7f6cde2dbc15c5f3da71
higorasilverio <higorasilverio@outlook.com>
2021-12-27 20:55:59 -0300

    💄 🚧 (mainPage): WIP - working with theming

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,35 @@
+import styled, { ThemeProvider } from 'styled-components'
+
+const Button = styled.button`
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border-radius: 3px;
+
+  /* Color the border and text with theme.main */
+  color: ${(props) => props.theme.main};
+  border: 2px solid ${(props) => props.theme.main};
+`
+
+Button.defaultProps = {
+  theme: {
+    main: 'palevioletred',
+  },
+}
+
+const theme = {
+  main: 'mediumseagreen',
+}
+
 export default function Home() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+  return (
+    <div>
+      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+      <Button>Normal</Button>
+
+      <ThemeProvider theme={theme}>
+        <Button>Themed</Button>
+      </ThemeProvider>
+    </div>
+  )
 }
