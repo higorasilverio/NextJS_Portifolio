bc691e1414b589a70b34527f1504412049f32496
higorasilverio <higorasilverio@outlook.com>
2021-12-28 20:06:38 -0300

    ♻️ (miscellaneous): code is formatted and linted

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,14 @@
-import { AppProps } from 'next/app'
 import '../styles/globals.css'
+import { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
 import { theme } from '../styles/theme'
 
 function App({ Component, pageProps }: AppProps) {
-	return (
-		<ThemeProvider theme={theme}>
-			<Component {...pageProps} />
-		</ThemeProvider>
-	)
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default App
--- styles/theme.ts
+++ styles/theme.ts
@@ -1,53 +1,53 @@
 const gridBasedSizes = {
-	xxs: '0rem',
-	xs: '0.25rem',
-	sm: '0.5rem',
-	md: '1rem',
-	lg: '1.5rem',
-	xl: '2rem',
-	xxl: '4rem',
+  xxs: '0rem',
+  xs: '0.25rem',
+  sm: '0.5rem',
+  md: '1rem',
+  lg: '1.5rem',
+  xl: '2rem',
+  xxl: '4rem'
 }
 
 export const theme = {
-	pallete: {
-		dark: {
-			100: '#B7C1C6',
-			300: '#1B2734',
-			500: '#0F1020',
-		},
-		light: {
-			100: '#FFFFFF',
-			300: '#0082FF',
-			500: '#02279E',
-		},
-		grey: {
-			100: '#F0F0F0',
-			300: '#CCCCCC',
-			500: '#333333',
-		},
-		success: {
-			100: '#01B36A',
-			300: '#01D17C',
-			500: '#019257',
-		},
-		error: {
-			100: '#FFCECE',
-			300: '#FF4949',
-			500: '#B33333',
-		},
-		warning: {
-			100: '#FFDCB1',
-			300: '#FF8B3D',
-			500: '#FF672D',
-		},
-	},
-	padding: {
-		...gridBasedSizes,
-	},
-	margin: {
-		...gridBasedSizes,
-	},
-	fontSize: {
-		...gridBasedSizes,
-	},
+  pallete: {
+    dark: {
+      100: '#B7C1C6',
+      300: '#1B2734',
+      500: '#0F1020'
+    },
+    light: {
+      100: '#FFFFFF',
+      300: '#0082FF',
+      500: '#02279E'
+    },
+    grey: {
+      100: '#F0F0F0',
+      300: '#CCCCCC',
+      500: '#333333'
+    },
+    success: {
+      100: '#01B36A',
+      300: '#01D17C',
+      500: '#019257'
+    },
+    error: {
+      100: '#FFCECE',
+      300: '#FF4949',
+      500: '#B33333'
+    },
+    warning: {
+      100: '#FFDCB1',
+      300: '#FF8B3D',
+      500: '#FF672D'
+    }
+  },
+  padding: {
+    ...gridBasedSizes
+  },
+  margin: {
+    ...gridBasedSizes
+  },
+  fontSize: {
+    ...gridBasedSizes
+  }
 }
