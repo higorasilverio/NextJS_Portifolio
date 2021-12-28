c459d197fb5916d9e431478c2b703416800a6d88
higorasilverio <higorasilverio@outlook.com>
2021-12-28 12:06:42 -0300

    👷 (miscellaneous): add other configurations

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,11 @@ import { ThemeProvider } from 'styled-components'
 import { theme } from '../styles/theme'
 
 function App({ Component, pageProps }: AppProps) {
-  return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
-  )
+	return (
+		<ThemeProvider theme={theme}>
+			<Component {...pageProps} />
+		</ThemeProvider>
+	)
 }
 
 export default App
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,7 @@
-import styled from 'styled-components'
-
-const Button = styled.button`
-  font-size: ${(props) => props.theme.fontSize.md};
-  margin: ${(props) => props.theme.fontSize.xl};
-  padding: ${(props) => props.theme.fontSize.md};
-  color: ${(props) => props.theme.pallete.success[100]};
-  border: 2px solid ${(props) => props.theme.pallete.success[500]};
-  border-radius: 10px;
-`
-
-const Header = styled.h1`
-  font-size: ${(props) => props.theme.fontSize.xl};
-  margin: ${(props) => props.theme.fontSize.xl};
-`
-
 export default function Home() {
-  return (
-    <div className='flex content-center'>
-      <Header>Hello world!</Header>
-      <Button>Themed</Button>
-    </div>
-  )
+	return (
+		<div className='flex w-full h-screen justify-center items-center bg-gradient-to-r from-sky-300 via-sky-200 to-sky-300'>
+			<h1>Hello world!</h1>
+		</div>
+	)
 }
