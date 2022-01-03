90523bb60cff2daa2f664bdeab2b8e79d9efeb03
higorasilverio <higorasilverio@outlook.com>
2022-01-03 19:53:16 -0300

    🚧 👽️ (github): start to work with github API

--- pages/github/index.tsx
+++ pages/github/index.tsx
@@ -1,14 +1,30 @@
+import { memo, useEffect, useState } from 'react'
 import HomeButton from '../../src/components/HomeButton'
+import { Octokit } from 'octokit'
 import Paper from '../../src/components/Paper'
 import Title from '../../src/components/Title'
 import Wrapper from '../../src/components/Wrapper'
-import { memo } from 'react'
 
-const Github = () => {
+
+const Github = ({ githubToken }) => {
+  const [name, setName] = useState<string>('loading...')
+
+  useEffect(() => {
+    const getLoginName = async () => {
+      const octokit = new Octokit({ auth: githubToken })
+      const {
+        data: { login }
+      } = await octokit.rest.users.getAuthenticated()
+      setName(login)
+    }
+    getLoginName()
+  }, [githubToken])
+
   return (
     <Wrapper>
       <Paper>
         <Title label="Github" />
+        <span>{name}</span>
       </Paper>
       <HomeButton />
     </Wrapper>
@@ -16,3 +32,13 @@ const Github = () => {
 }
 
 export default memo(Github)
+
+export const getServerSideProps = () => {
+  // eslint-disable-next-line no-undef
+  const githubToken = process.env.GITHUB_TOKEN
+  return {
+    props: {
+      githubToken
+    }
+  }
+}
